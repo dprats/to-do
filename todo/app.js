b5d611c2e723cc066cf1dafd0798868b2da67057
Diego Prats <diego.prats@gmail.com>
2014-02-13 21:16:09 -0800

    finish tutorial, cannot create

--- todo/app.js
+++ todo/app.js
@@ -21,10 +21,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(express.cookieParser());
 app.use( express.bodyParser());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use( routes.current_user);
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -37,6 +39,9 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/create', routes.create);
+app.get('/destroy:id', routes.destroy);
+app.get('/edit/:id', routes.edit);
+app.post('/update/:id', routes.update);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- todo/routes/index.js
+++ todo/routes/index.js
@@ -2,24 +2,98 @@
 /*
  * GET home page.
  */
-
- var mongoose = require('mongoose');
- var Todo = mongoose.model('Todo');
-
-exports.index = function(req, res){
-		Todo.find( function (err, todos, count){
-			res.render('index', {
-				title: 'Express todo Example',
-				todos: todos
-			});
-		});
+var utils = require('../utils');
+var mongoose = require('mongoose');
+var Todo = mongoose.model('Todo');
+exports.index = function ( req, res, next ){
+  var user_id = req.cookies ?
+    req.cookies.user_id : undefined;
+ 
+  Todo.
+    find({ user_id : user_id }).
+    sort( '-updated_at' ).
+    exec( function ( err, todos ){
+      if( err ) return next( err );
+ 
+      res.render( 'index', {
+          title : 'Express Todo Example',
+          todos : todos
+      });
+    });
+};
+ 
+exports.create = function ( req, res, next ){
+  new Todo({
+      user_id    : req.cookies.user_id,
+      content    : req.body.content,
+      updated_at : Date.now()
+  }).save( function ( err, todo, count ){
+    if( err ) return next( err );
+ 
+    res.redirect( '/' );
+  });
+};
+ 
+exports.destroy = function ( req, res, next ){
+  Todo.findById( req.params.id, function ( err, todo ){
+    var user_id = req.cookies ?
+      req.cookies.user_id : undefined;
+ 
+    if( todo.user_id !== req.cookies.user_id ){
+      return utils.forbidden( res );
+    }
+ 
+    todo.remove( function ( err, todo ){
+      if( err ) return next( err );
+ 
+      res.redirect( '/' );
+    });
+  });
+};
+ 
+exports.edit = function( req, res, next ){
+  var user_id = req.cookies ?
+      req.cookies.user_id : undefined;
+ 
+  Todo.
+    find({ user_id : user_id }).
+    sort( '-updated_at' ).
+    exec( function ( err, todos ){
+      if( err ) return next( err );
+ 
+      res.render( 'edit', {
+        title   : 'Express Todo Example',
+        todos   : todos,
+        current : req.params.id
+      });
+    });
+};
+ 
+exports.update = function( req, res, next ){
+  Todo.findById( req.params.id, function ( err, todo ){
+    var user_id = req.cookies ?
+      req.cookies.user_id : undefined;
+ 
+    if( todo.user_id !== user_id ){
+      return utils.forbidden( res );
+    }
+ 
+    todo.content    = req.body.content;
+    todo.updated_at = Date.now();
+    todo.save( function ( err, todo, count ){
+      if( err ) return next( err );
+ 
+      res.redirect( '/' );
+    });
+  });
 };
 
-exports.create = function(req, res){
-	new Todo({
-		content: req.body.content,
-		updated_at: Date.now()
-	}).save (function(err, todo, count){
-		res.redirect('/');
-	})
-};
+exports.current_user = function( req, res, next){
+	var user_id = req.cookies ?
+		req.cookies.user_id : undefined;
+
+	if (!user_id){
+		res.cookies('user_id', utils.uid( 32));
+	}
+	next();
+}

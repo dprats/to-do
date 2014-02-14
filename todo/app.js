05befd8ea6ce0aba5fd7fd8a035886871efa7e86
Diego Prats <diego.prats@gmail.com>
2014-02-13 20:45:28 -0800

    remove index.jade, layout.jade, user.js, create layout.ejs, index.ejs, db.js

--- todo/app.js
+++ todo/app.js
@@ -3,20 +3,25 @@
  * Module dependencies.
  */
 
+require('./db');
+
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+// var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var engine = require('ejs-locals');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
+app.engine('ejs', engine);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use( express.bodyParser());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
@@ -28,8 +33,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//ROUTES
+
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/create', routes.create);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- todo/routes/index.js
+++ todo/routes/index.js
@@ -3,6 +3,23 @@
  * GET home page.
  */
 
+ var mongoose = require('mongoose');
+ var Todo = mongoose.model('Todo');
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+		Todo.find( function (err, todos, count){
+			res.render('index', {
+				title: 'Express todo Example',
+				todos: todos
+			});
+		});
+};
+
+exports.create = function(req, res){
+	new Todo({
+		content: req.body.content,
+		updated_at: Date.now()
+	}).save (function(err, todo, count){
+		res.redirect('/');
+	})
 };
